aa5a3fb7b4d67c19ab1f2df9a941c91579e7dc27
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-14 08:15:02 -0500

    Updated from Brython Server: 2/14/2018 8:15:00 AM

--- loopDemo.py
+++ loopDemo.py
@@ -2,6 +2,12 @@
 #2/14/18
 #loopDemo.py - how to use for and while loops
 
+"""
 #I love comp sci 5 times
-for i in range(1,5):
+for i in range(1,6):
     print('I <3 Computer Science')
+"""
+
+#Counts from 1 to 10
+for i in range(1,11):
+    print(i)
