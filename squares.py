701ed6b6ca66a0b6deff77339ac32a1807621f9f
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-14 08:38:49 -0500

    Updated from Brython Server: 2/14/2018 8:38:48 AM

--- squares.py
+++ squares.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#2/14/18
+#squares.py - asks user for number prints out that many square numbers starting with 1
 
+num = int(input('Enter a number: '))
+
+
+
+for i in range(1,num+1):
+    print(i**2)
