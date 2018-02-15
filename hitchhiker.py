ba2e650ed64b16c686ba98a42013f082fc5a0e3a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-15 08:42:43 -0500

    Updated from Brython Server: 2/15/2018 8:42:41 AM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#2/15/18
+#hitchhiker.py - prompts user to ask question or type quit
 
+while True:
+    ans = input('Ask a question or type "quit": ')
+    if ans == 'quit':
+        break
+    else:
+        print(42)
+    
