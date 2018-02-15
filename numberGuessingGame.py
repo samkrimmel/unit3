aedf78a60021806e3bf200a21ecb4d75da4c08aa
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-15 14:15:04 -0500

    Updated from Brython Server: 2/15/2018 2:15:01 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -7,5 +7,11 @@ num = randint(1,100)
 
 while True:
     guess = int(input('Guess a number! '))
+    if guess == num:
+        print('You guessed it!')
+    elif guess > num:
+        print('Too high!')
+    else:
+        print('Too low!')
     
 
