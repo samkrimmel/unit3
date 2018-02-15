645277faa7163ea29d33f763bd8c26147e7dee22
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-15 08:13:46 -0500

    Updated from Brython Server: 2/15/2018 8:13:44 AM

--- countdown.py
+++ countdown.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#2/15/18
+#countdown.py - counts down to zero and then prints BOOM
 
+for i in range (5,-1,-1):
+    if i == 0:
+        print('BOOM!')
+    else:
+        print(i)
+
+    
