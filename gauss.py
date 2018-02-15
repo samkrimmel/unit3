f38041acc5588237caa308601a3e47c4453f80c7
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-15 08:31:51 -0500

    Updated from Brython Server: 2/15/2018 8:31:49 AM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#2/15/18
+#gauss.py - adds up all numbers in a range with a difference
 
+low = int(input('Enter the lowest number in the range: '))
+high = int(input('Enter the highest number in the range: '))
+diff = int(input('Enter a difference: '))
+total = 0
+for i in range(low,high+1,diff):
+    total += i
+print('The sum of the numbers from', low,'to', high,'is', total)
