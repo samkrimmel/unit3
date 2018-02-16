8eac975b05e1ba49514d01a3be65e34954722237
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-16 08:26:07 -0500

    Updated from Brython Server: 2/16/2018 8:26:04 AM

--- warmup6.py
+++ warmup6.py
@@ -2,11 +2,11 @@
 #2/16/18
 #warmup6.py - counts mississippis
 
-for i in range(1,12):
+for i in range(1,11):
     print(i,'mississippi')
     
 num = 1
 
-while num <= 11:
+while num <= 10:
     print(num,'mississippi')
     num += 1
