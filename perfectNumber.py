d7475f72563b3a10afbe63f1ee8e0b586a6de58e
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-26 08:29:51 -0500

    Updated from Brython Server: 2/26/2018 8:29:46 AM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,16 @@
+#Sam Krimmel
+#2/26/18
+#perfectNumber.py - tells whether an entered number is perfect or not
 
+num = int(input('Enter a number: '))
+
+total = 0
+
+for i in range(1,num):
+    if num%i == 0:
+        total += i
+
+if total == num:
+    print('Perfect!')
+else:
+    print('Not perfect...')
