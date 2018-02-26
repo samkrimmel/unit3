49b0f2c5b0b96e45ad8a3e89faf133063bea7544
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-26 08:23:44 -0500

    Updated from Brython Server: 2/26/2018 8:23:39 AM

--- divisors.py
+++ divisors.py
@@ -1,1 +1,10 @@
+#Sam Krimmel
+#2/26/18
+#divisors.py - asks user for number and prints all divisors
 
+num = int(input('Enter a number: '))
+
+for i in range(1,num):
+    if num%i == 0:
+        print(i)
+    
