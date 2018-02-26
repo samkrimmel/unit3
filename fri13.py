d23da2ed5b81cfb73af4319ff62c6e2af2a9eb27
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-26 08:48:43 -0500

    Updated from Brython Server: 2/26/2018 8:48:37 AM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,24 @@
+#Sam Krimmel
+#2/26/18
+#fri13.py - prints the next ten friday the 13ths
 
+from calendar import weekday
+from datetime import date
+
+year = date.today().year
+month = date.today().month
+day = date.today().day
+
+frinum = 0
+
+while frinum < 10:
+    break
+
+if weekday() == 4 and day == 13:
+    print(month/day/year)
+    frinum += 1
+
+if date.today().day > 13:
+    if weekday(year,month,13) == 4:
+        print(month,"/",13,"/",year)
+    
