9430beb84009cb07aeb57a9aaf506b53bae06596
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-28 08:27:17 -0500

    Updated from Brython Server: 2/28/2018 8:27:16 AM

--- warmup8.py
+++ warmup8.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#2/28/18
+#warmup8.py - Find sum if all number less than 100,000 that are divisible bu 3, 10 , and 17
 
+sum = 0
+
+for i in range(1,100001):
+    if i%3 == 0 and i%10 == 0 and i%17 == 0:
+        sum += i
+
+print(sum)
