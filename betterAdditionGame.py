0f319148b206983a99b74fe6053e5aaaba38875a
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-02-28 08:15:55 -0500

    Updated from Brython Server: 2/28/2018 8:15:54 AM

--- betterAdditionGame.py
+++ betterAdditionGame.py
@@ -1,1 +1,11 @@
+#Sam Krimmel
+#2/28/18
+#betterAdditionGameDemo.py - more loop practice
 
+from random import randint
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(-10,10)
+    num2 = randint(-10,10)
+    input('What is ' + str(num1) + '+' + str(num2) + '? ')
