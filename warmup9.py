577412f81373db29231cbd475e54c0d9fc4a95cd
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-01 08:26:27 -0500

    Updated from Brython Server: 3/1/2018 8:26:26 AM

--- warmup9.py
+++ warmup9.py
@@ -1,1 +1,12 @@
+#Sam Krimmel
+#3/1/18
+#warmup9.py - asks user to input some text and prints it with vowels capitalized
 
+text = input('Enter text: ')
+
+for ch in text:
+    ch = ch.lower()
+    if ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+        print(ch.upper())
+    else:
+        print(ch.lower())
