32946288f5d4c5f2faf665ac64887f5da4e73163
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-08 17:57:18 -0500

    Updated from Brython Server: 3/8/2018 5:57:14 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,18 @@
+#Sam Krimmel
+#3/8/18
+#changeComputerLoop.py - asks user for number of cents and outputs change
+
+change = int(input('Enter change in cents: '))
+
+for i in range(1,10):
+    quar = cash//25
+    dime = (cash - 25*quar)//10
+    nick = (cash - (dime*10 + quar*25))//5
+    penn = cash - (dime*10 + quar*25 + nick*5)
+
+print('Quarters: ', quar)
+print('Dimes: ', dime)
+print('Nickels: ', nick)
+print('Pennies: ', penn)
+
 
